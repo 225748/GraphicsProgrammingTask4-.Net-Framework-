adf0434fc119c78e1947983157d002b95325cb2e
Ben Franklin <225748@live.brock.ac.uk>
2024-06-10 21:35:17 +0100

    Add project files.

---
+++ Mover.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+using System.Data.Common;
+using System;
+using System.Drawing;
+
+namespace GraphicsProgrammingTask4_.Net_Framework_
+{
+    public class Mover
+    {
+        static Random rnd = new Random();
+        static Random startRandLocation = new Random();
+        Vector2 location;
+        Vector2 velocity;
+        int formWidth, formHeight;
+
+        public Mover(int width, int height)
+        {
+            formWidth = width;
+            formHeight = height;
+            location = new Vector2(startRandLocation.Next(width),startRandLocation.Next(height)); //locates it in the middle of the screen
+            velocity = new Vector2(3, 2);  //ratio of horizontal to verticle movement
+        }
+
+        public void Update()
+        {
+            if (location.X < 0 || location.X > formWidth - 140)
+            {
+                velocity.X = velocity.X * -1;
+            }
+            if (location.Y < 0 || location.Y > formHeight - 140)
+            {
+                velocity.Y = velocity.Y * -1;
+            }
+            location = Vector2.Add(location, velocity);
+
+
+        }
+
+        public void Display(Graphics e)
+        {
+            e.FillEllipse(Brushes.BlueViolet, location.X, location.Y, 100, 100);
+        }
+
+
+    }
+
+}
---
+++ frmBBTask4.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsProgrammingTask4_.Net_Framework_
+{
+    public partial class frmBBTask4 : Form
+    {
+        Mover mover; //sigle mover //declared outside for global access
+        List<Mover> movers = new List<Mover>(); // multiple movers
+
+
+        public frmBBTask4()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.DoubleBuffered = true;
+            Timer timer1 = new Timer();
+            timer1.Interval = 10;
+            timer1.Enabled = true;
+            timer1.Tick += Timer1_Tick;
+            this.Paint += Form1_Paint;
+
+            //this is a very clever way to do it, list dynamically grows with demand for movers
+            for (int i = 0; i < 5; i++) // change i number for more/less movers
+            {
+                mover = new Mover(this.Width, this.Height);
+                movers.Add(mover);
+
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //mover.Update();
+            //mover.Display(e.Graphics);
+            foreach (var item in movers)
+            {
+                item.Update();
+                item.Display(e.Graphics);
+            }
+
+        }
+    }
+}
